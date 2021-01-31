fe9a9b68b1ae1bb42addf8a9a34a540fd6a337b5
StroupBSlayen <29642143+stroupbslayen@users.noreply.github.com>
2021-01-31 13:24:23 -0800

    ADD: test script

--- tests/testbot.py
+++ tests/testbot.py
@@ -3,7 +3,7 @@ import os
 import dotenv
 from discord.ext import commands
 
-dotenv.load_dotenv()
+dotenv.load_dotenv("./tests/.env")
 
 bot = commands.Bot(command_prefix=os.environ["COMMAND_PREFIX"], case_insensitive=True)
 
@@ -15,4 +15,5 @@ async def on_ready():
     print(f"Logged in as: {bot.user}\nBot ID: {bot.user.id}")
 
 
-bot.run(os.environ["DISCORD_TOKEN"])
+def run():
+    bot.run(os.environ["DISCORD_TOKEN"])
