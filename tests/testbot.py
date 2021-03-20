0f2c02819082de1d997a697d258b0140167022b9
StroupBSlayen <29642143+stroupbslayen@users.noreply.github.com>
2021-03-20 14:27:47 -0700

    Add: Intents for test bot

--- tests/testbot.py
+++ tests/testbot.py
@@ -1,11 +1,14 @@
 import os
 
 import dotenv
+import discord
 from discord.ext import commands
 
 dotenv.load_dotenv("./tests/.env")
 
-bot = commands.Bot(command_prefix="!", case_insensitive=True)
+bot = commands.Bot(
+    command_prefix="!", case_insensitive=True, intents=discord.Intents.all()
+)
 
 bot.load_extension("gamerdb.gamerdb")
 
