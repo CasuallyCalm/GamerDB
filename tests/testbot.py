d3d9c5a4623803911460bf13c57513128f10f934
StroupBSlayen <29642143+stroupbslayen@users.noreply.github.com>
2022-08-28 10:45:59 -0700

    update testing

--- tests/testbot.py
+++ tests/testbot.py
@@ -1,22 +1,40 @@
+import contextlib
 import os
 
 import dotenv
 import discord
+from discord import app_commands
 from discord.ext import commands
 
 dotenv.load_dotenv("./tests/.env")
 
-bot = commands.Bot(
-    command_prefix="!", case_insensitive=True, intents=discord.Intents.all()
-)
+intents= discord.Intents.all()
 
-bot.load_extension("gamerdb.gamerdb")
+class TestBot(commands.Bot):
 
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.test_guild = discord.Object(id=os.environ.get('GUILD_ID'))
+        self.extension_path='gamerdb.gamerdb'
 
-@bot.event
-async def on_ready():
-    print(f"Logged in as: {bot.user}\nBot ID: {bot.user.id}")
+    async def setup_hook(self) -> None:
+        await self.load()
+        print(f"Logged in as: {self.user}\nBot ID: {self.user.id}")
 
+    async def load(self):
+        await self.load_extension(self.extension_path)
+        self.tree.copy_global_to(guild=self.test_guild)
+        await self.tree.sync(guild=self.test_guild)
+    
+bot = TestBot(commands.when_mentioned, intents=intents)
+
+@bot.tree.command()
+async def reload(interaction: discord.Interaction):
+    client = interaction.client
+    with contextlib.suppress(Exception):
+        await client.unload_extension(client.extension_path)
+    await client.load()
+    await interaction.response.send_message('reloaded', ephemeral=True)
 
 def run():
     bot.run(os.environ["DISCORD_TOKEN"])
